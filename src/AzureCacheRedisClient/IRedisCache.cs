3f7973d8536d41a080830a027603ab757e6a3abd
Daniel Larsen <dalars@microsoft.com>
2022-03-02 18:09:24 +1300

    +IRedisCache

---
+++ src/AzureCacheRedisClient/IRedisCache.cs
@@ -0,0 +1,31 @@
+﻿
+namespace AzureCacheRedisClient
+{
+    public interface IRedisCache
+    {
+        /// <summary>
+        /// Adds a value to Cache, only if no key with the same name exists.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="expiry"></param>
+        /// <returns>`true` if the value was set. `false` if the key already exists.</returns>
+        Task<bool> Add(string key, object value, TimeSpan expiry);
+
+        /// <summary>
+        /// Returns a cache item by key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns>The value or null if no key exists</returns>
+        Task<T?> Get<T>(string key);
+
+        /// <summary>
+        /// Sets a value in Cache, overwriting any existing value if same key exists.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="expiry"></param>
+        Task Set(string key, object value, TimeSpan expiry);
+    }
+}
