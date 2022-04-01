8af2a5007aec179e3e7d2bf54605d2765206f9ff
Daniel Larsen <dalars@microsoft.com>
2022-04-01 13:52:11 +1300

    Task waitall sample

---
+++ samples/TaskWaitAll/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionMultiplexer.Connect(
+                        "helloaspnet-eus2-redis.redis.cache.windows.net:6380,password=D6F429jOBaCodRZOvOpsfLcRmu7QKuk8WAzCaBgzM4w=,ssl=True,abortConnect=False",
+                        configure: new Action<ConfigurationOptions>(options =>
+                        {
+                            options.AbortOnConnectFail = false;
+
+                        }));
+
